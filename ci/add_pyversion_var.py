851574c5d5181e5b5bc891bd06890b40fd5c0e83
Charles Machalow <csm10495@gmail.com>
2019-08-02 23:03:01 -0700

    small change

--- ci/add_pyversion_var.py
+++ ci/add_pyversion_var.py
@@ -1,2 +1,4 @@
 import sys
-print('##vso[task.setvariable variable=PyVersion]%s_%s' % (sys.version_info.major, sys.version_info.minor))
+
+if __name__ == '__main__':
+    print('##vso[task.setvariable variable=PyVersion]%s_%s' % (sys.version_info.major, sys.version_info.minor))
